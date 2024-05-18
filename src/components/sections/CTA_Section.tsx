b2539cc404b623b83d3061811a1b3f1a42bf01bf
Ahmed Mohamed <barmajli.info@gmail.com>
2024-05-18 21:56:01 +0300

    add CAT section

--- src/components/sections/CTA_Section.tsx
+++ src/components/sections/CTA_Section.tsx
@@ -1,10 +1,16 @@
+import Button from "../../common/Button"
 
 const CTA_Section = () => {
   return (
-    <section>
-      <h1>CTA_Section </h1>
+    <section className="rounded-3xl bg-white flex flex-col sm:flex-row items-center justify-between py-10 px-6 sm:p-10">
+      <div className="text-center sm:text-start">
+        <h2 className="text-2xl sm:text-3xl lg:text-5xl ">Simplifying Complexity with Innovative Solutions</h2>
+        <Button className="mt-3 w-full sm:w-fit">Get Started</Button>
+      </div>
+      <img src="CTA.png" alt="CTA.png" className="w-60 lg:w-auto py-8 sm:py-0 "/>
     </section>
   )
 }
 
 export default CTA_Section
+0
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import About_Section from "../components/sections/About_Section"
+import Blog_Section from "../components/sections/Blog_Section"
+import CTA_Section from "../components/sections/CTA_Section"
 import Hero from "../components/sections/Hero"
 import Investing_Section from "../components/sections/Investing_Section"
 import Solution_Section from "../components/sections/Solution_Section"
@@ -11,7 +13,8 @@ const Home = () => {
       <About_Section />
       <Solution_Section/>
       <Investing_Section/>
-      {/* <Blog_Section/> */}
+      <Blog_Section/>
+      <CTA_Section/>
     </>
   )
 }
