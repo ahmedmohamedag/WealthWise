17ca8a190b46ece5bee702c6d54b7b03b4efd115
Ahmed Mohamed <barmajli.info@gmail.com>
2024-05-20 20:32:50 +0300

    add contact link in navoverlay at sm:Screen

--- src/components/Navbar_Overlay.tsx
+++ src/components/Navbar_Overlay.tsx
@@ -3,15 +3,21 @@ import { navLinkes } from "../data"
 
 const Navbar_Overlay = () => {
     return (
-        <ul id="Navbar_Overlay" className="fixed top-[100px] bottom-0 left-0 right-0 z-30 flex flex-col items-center justify-center gap-4  md:hidden">
-            {
-                navLinkes.map(link => (
-                    <li className="" key={link.id}>
-                        <Link to={link.path} className="text-xl tracking-wide capitalize font-xl hover:text-light-blue ">{link.title}</Link>
-                    </li>
-                ))
-            }
-        </ul>
+        <>
+            <ul id="Navbar_Overlay" className="fixed top-[100px] bottom-0 left-0 right-0 z-30 flex flex-col items-center justify-center gap-4  md:hidden">
+                {
+                    navLinkes.map(link => (
+                        <li className="" key={link.id}>
+                            <Link to={link.path} className="text-xl tracking-wide capitalize font-xl hover:text-light-blue ">{link.title}</Link>
+                        </li>
+                    ))
+                }
+                <li>
+                    <Link to="/contact" className="text-xl tracking-wide capitalize font-xl hover:text-light-blue ">Contact</Link>
+                </li>
+
+            </ul>
+        </>
     )
 }
 
